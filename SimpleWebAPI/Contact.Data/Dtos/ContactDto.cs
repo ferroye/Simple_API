a375cf742905a7165c85b8ff25f5b500254bd615
Ye <yewang8864@gmail.com>
2019-02-14 14:46:55 -0500

    need to work on connection string

---
+++ SimpleWebAPI/Contact.Data/Dtos/ContactDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Data.Dtos
+{
+    public class ContactDto
+    {
+        public string contactName { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+        public int zipcode { get; set; }
+        public string contactNote { get; set; }
+    }
+}
---
+++ SimpleWebAPI/Contact.Data/Entities/Contact.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contact.Data.Entities
+{
+    public class Contact
+    {
+        public string contactName { get; set; }
+        public string address { get; set; }
+        public string phoneNumber { get; set; }
+        public int zipcode { get; set; }
+    }
+}
